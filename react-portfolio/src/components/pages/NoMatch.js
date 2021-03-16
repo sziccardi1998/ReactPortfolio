df992461b073bcdf4c68a28b58346c01c9ce3af8
sziccardi1998 <sziccardifullstack@gmail.com>
2021-03-15 20:39:26 -0400

    error page.

---
+++ react-portfolio/src/components/pages/NoMatch.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+function NoMatch() {
+  return (
+    <div>
+    
+            <h1>Oh no! You made a wrong turn!</h1>
+            <h1>
+              <p>404-No Page Found</p>
+                
+             
+            </h1>
+        
+    </div>
+  );
+}
+
+export default NoMatch;
