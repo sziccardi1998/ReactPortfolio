ed17e422405eb79bc377fac10cea841cf9f3b93d
sziccardi1998 <sziccardifullstack@gmail.com>
2021-03-15 20:54:54 -0400

    Homepage.

---
+++ react-portfolio/src/components/pages/Home/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./home.css";
+import Nav from "../../Nav"
+
+const Home = () => {
+  return(
+  <div id="landing">
+   <img src="" id="hero" alt=""/>
+   <h2>full-stack web developer</h2>
+  </div>
+  )};
+
+export default Home;
